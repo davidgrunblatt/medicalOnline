3320eb3f22fbf937de09018284acd0859b3a4c8c
davidgrunblatt <dpg1919@mgail.com>
2019-12-25 19:46:37 -0800

    build file

---
+++ client/build/precache-manifest.2a51052a634048530794e80c95811ab4.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9defd4f1fd97c879f334de58dae35e71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18e43df3231dfa02a007",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "ecac4bbcdfad38c201be",
+    "url": "/static/css/main.3216215c.chunk.css"
+  },
+  {
+    "revision": "18e43df3231dfa02a007",
+    "url": "/static/js/2.0fcf3626.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.0fcf3626.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ecac4bbcdfad38c201be",
+    "url": "/static/js/main.2c57e7a7.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  },
+  {
+    "revision": "b3df17c06902ec7be51d36cc8bf79ee3",
+    "url": "/static/media/castillo.b3df17c0.jpg"
+  },
+  {
+    "revision": "cbdd4dc77bda841c5b186ff2a17be82a",
+    "url": "/static/media/four.cbdd4dc7.jpg"
+  },
+  {
+    "revision": "cfd14ba7b601aa88c5c333ee4979b24b",
+    "url": "/static/media/logo.cfd14ba7.png"
+  },
+  {
+    "revision": "b2fb57f84aa89b1da60ec1ae92c2c80c",
+    "url": "/static/media/logo_full.b2fb57f8.png"
+  },
+  {
+    "revision": "a9221904e197f370e8bd86805626233e",
+    "url": "/static/media/madrid.a9221904.jpg"
+  },
+  {
+    "revision": "bd16ca03fd54d7a7ad11c715bf1ccca6",
+    "url": "/static/media/mapa.bd16ca03.jpg"
+  },
+  {
+    "revision": "a2297d0683fcc8172d564c8fa1a1d73b",
+    "url": "/static/media/one.a2297d06.jpg"
+  },
+  {
+    "revision": "63d82946ab79d8379a8592bb2ef05311",
+    "url": "/static/media/paris.63d82946.jpg"
+  },
+  {
+    "revision": "ad098096be5453e2f02f16794fe8c296",
+    "url": "/static/media/shanghai.ad098096.jpg"
+  },
+  {
+    "revision": "7af8e083e408b41cc8a5dca14e421c8f",
+    "url": "/static/media/slogan.7af8e083.png"
+  },
+  {
+    "revision": "a2bd4bc80505d9c333fe7b6ebf037288",
+    "url": "/static/media/three.a2bd4bc8.jpg"
+  },
+  {
+    "revision": "fb835b17894e47a887d549cde6ce6ac7",
+    "url": "/static/media/two.fb835b17.jpg"
+  }
+]);
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ if(!process.env.jwtPrivateKey) throw new Error((err) => console.log('JWT Key is 
 require('./startup/routes')(app); // ROUTES 
 
 // EXPRESS.STATIC - PUBLIC FOLDER 
-// app.use(express.static('./public'));
+app.use(express.static('./public'));
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('./client/build')); 
