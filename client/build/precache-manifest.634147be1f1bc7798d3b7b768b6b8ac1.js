2cc31f16e870fa6a9dd4b9acce0690639ecb6864
davidgrunblatt <dpg1919@mgail.com>
2019-12-27 21:40:43 -0800

    new react build to push to heroku

---
+++ client/build/precache-manifest.634147be1f1bc7798d3b7b768b6b8ac1.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45b9ac81ffcccdb8149f164a9ea9c23c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "177b287be02766dc8c58",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "d0b850450c23a5d7f16c",
+    "url": "/static/css/main.79f19bcc.chunk.css"
+  },
+  {
+    "revision": "177b287be02766dc8c58",
+    "url": "/static/js/2.3fd334ba.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.3fd334ba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d0b850450c23a5d7f16c",
+    "url": "/static/js/main.b1efc4c5.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  },
+  {
+    "revision": "cbdd4dc77bda841c5b186ff2a17be82a",
+    "url": "/static/media/four.cbdd4dc7.jpg"
+  },
+  {
+    "revision": "4f1652b89c1162bcf4c062012c23f8b7",
+    "url": "/static/media/login.4f1652b8.png"
+  },
+  {
+    "revision": "cfd14ba7b601aa88c5c333ee4979b24b",
+    "url": "/static/media/logo.cfd14ba7.png"
+  },
+  {
+    "revision": "a9221904e197f370e8bd86805626233e",
+    "url": "/static/media/madrid.a9221904.jpg"
+  },
+  {
+    "revision": "a2297d0683fcc8172d564c8fa1a1d73b",
+    "url": "/static/media/one.a2297d06.jpg"
+  },
+  {
+    "revision": "63d82946ab79d8379a8592bb2ef05311",
+    "url": "/static/media/paris.63d82946.jpg"
+  },
+  {
+    "revision": "ad098096be5453e2f02f16794fe8c296",
+    "url": "/static/media/shanghai.ad098096.jpg"
+  },
+  {
+    "revision": "298ea34e7689ad9cc4f7252ad9d50d94",
+    "url": "/static/media/three.298ea34e.jpg"
+  },
+  {
+    "revision": "38354f98bc4824a3634b147994a00135",
+    "url": "/static/media/two.38354f98.jpg"
+  }
+]);
