f992f47558422105d90aded027434379d1890ea3
davidgrunblatt <dpg1919@mgail.com>
2019-12-27 21:55:16 -0800

    port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 // EXPRESS SETUP 
 const express = require('express');
 const app = express();
-const port = process.env.port || 3001;
+const port = process.env.PORT || 3001;
 const mongoose = require('./startup/mongoose');
 
 // IF JWT PRIVATE KEY IS NOT INITIALIZED THROW ERROR 
@@ -12,7 +12,7 @@ if(!process.env.jwtPrivateKey) throw new Error((err) => console.log('JWT Key is 
 require('./startup/routes')(app); // ROUTES 
 
 // EXPRESS.STATIC - PUBLIC FOLDER 
-app.use(express.static('./public'));
+// app.use(express.static('./public'));
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('/client/build')); 
