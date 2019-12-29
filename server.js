c34a574bcddae5cfd433594299cd52117808fa86
davidgrunblatt <dpg1919@mgail.com>
2019-12-29 00:52:10 -0800

    changed express static folder for production

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ if(!process.env.jwtPrivateKey) throw new Error((err) => console.log('JWT Key is 
 // STARTUP
 require('./startup/routes')(app); // ROUTES 
 
-app.use(express.static('./public')); 
+// app.use(express.static('./public')); 
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('./client/build')); 
