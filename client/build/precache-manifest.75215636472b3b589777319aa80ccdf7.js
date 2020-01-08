73581603d865a2b1572cd68755dc26b883a93a61
davidgrunblatt <dpg1919@mgail.com>
2020-01-07 19:01:04 -0800

    new build with footer section

---
+++ client/build/precache-manifest.75215636472b3b589777319aa80ccdf7.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b9b0cf24d31cdf18ea3500d4d78ac13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9097647ffc53e3b6c5d1",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "7f9c918ab677a211e102",
+    "url": "/static/css/main.bdfbaa7c.chunk.css"
+  },
+  {
+    "revision": "9097647ffc53e3b6c5d1",
+    "url": "/static/js/2.48126405.chunk.js"
+  },
+  {
+    "revision": "e75ac3766afe8949dbc007d008031d8b",
+    "url": "/static/js/2.48126405.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f9c918ab677a211e102",
+    "url": "/static/js/main.0fee338e.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  },
+  {
+    "revision": "79260b98991df9f65a933604ff69347f",
+    "url": "/static/media/conferencia.79260b98.jpg"
+  },
+  {
+    "revision": "251cb2aa501ec1a3b209195054bdc81d",
+    "url": "/static/media/contact.251cb2aa.png"
+  },
+  {
+    "revision": "80a54b5bd989943f985c2c9519abbc13",
+    "url": "/static/media/doctor-icon.80a54b5b.png"
+  },
+  {
+    "revision": "b6a46107196bde548e414e695083ac7a",
+    "url": "/static/media/facebook.b6a46107.png"
+  },
+  {
+    "revision": "cbdd4dc77bda841c5b186ff2a17be82a",
+    "url": "/static/media/four.cbdd4dc7.jpg"
+  },
+  {
+    "revision": "b00c10b3a338cfb4c0f0940b6dc79de9",
+    "url": "/static/media/home.b00c10b3.png"
+  },
+  {
+    "revision": "4f1652b89c1162bcf4c062012c23f8b7",
+    "url": "/static/media/login.4f1652b8.png"
+  },
+  {
+    "revision": "cfd14ba7b601aa88c5c333ee4979b24b",
+    "url": "/static/media/logo.cfd14ba7.png"
+  },
+  {
+    "revision": "e2eb3b2cdbeb8ca325605c4c9e9f3f8b",
+    "url": "/static/media/logout.e2eb3b2c.png"
+  },
+  {
+    "revision": "a9221904e197f370e8bd86805626233e",
+    "url": "/static/media/madrid.a9221904.jpg"
+  },
+  {
+    "revision": "bd16ca03fd54d7a7ad11c715bf1ccca6",
+    "url": "/static/media/mapa.bd16ca03.jpg"
+  },
+  {
+    "revision": "9db73639a3445807088eae478beaf417",
+    "url": "/static/media/mapa_mobile.9db73639.png"
+  },
+  {
+    "revision": "8385e1b34ca198dd4bd107795bf171d0",
+    "url": "/static/media/next.8385e1b3.png"
+  },
+  {
+    "revision": "a2297d0683fcc8172d564c8fa1a1d73b",
+    "url": "/static/media/one.a2297d06.jpg"
+  },
+  {
+    "revision": "63d82946ab79d8379a8592bb2ef05311",
+    "url": "/static/media/paris.63d82946.jpg"
+  },
+  {
+    "revision": "ad098096be5453e2f02f16794fe8c296",
+    "url": "/static/media/shanghai.ad098096.jpg"
+  },
+  {
+    "revision": "dd15be3ea6b1690ea0edb9cbe7a2edcf",
+    "url": "/static/media/tenerife.dd15be3e.jpg"
+  },
+  {
+    "revision": "298ea34e7689ad9cc4f7252ad9d50d94",
+    "url": "/static/media/three.298ea34e.jpg"
+  },
+  {
+    "revision": "38354f98bc4824a3634b147994a00135",
+    "url": "/static/media/two.38354f98.jpg"
+  }
+]);
